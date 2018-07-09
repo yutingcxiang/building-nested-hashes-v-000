770d79260cb114e0e8659a8555015a46884b63eb
Christine Xiang <yutingcxiang@gmail.com>
2018-07-09 20:19:07 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,7 +2,8 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
+        :name => "Lord Montague",
+        :age => 53
         },
       :matriarch => {
         #your key/value pairs here
