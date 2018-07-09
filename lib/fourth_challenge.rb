3a7ad6c395f31064e339e0fbb7b765d253cf8aae
Christine Xiang <yutingcxiang@gmail.com>
2018-07-09 20:30:11 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,7 +5,7 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        {:name => "Benvolio", :age => "17", :attitude => "worried"}, {:name => "Mercutio", :age => "17", :attitude => "hot-headed"}
+        {name: "Benvolio", age: "17", attitude: "worried"}, {name: "Mercutio", age: "17", attitude: "hot-headed"}
       ]
    }, 
    :capulet => {
